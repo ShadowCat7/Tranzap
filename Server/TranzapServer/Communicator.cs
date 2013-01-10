c3488a771c789ae841ec5beed0a56fa992d77cca
superluke <lukasattridge@gmail.com>
2013-01-10 15:53:09 -0700

    First commit. This is nothing right now.

---
+++ Server/TranzapServer/Communicator.cs
@@ -0,0 +1,57 @@
+﻿using System.IO;
+using System.Net;
+using System.Collections.Generic;
+using System;
+using System.Threading;
+using System.Collections;
+using System.Net;
+
+namespace InstantMessenger
+{
+    public class Communicator
+    {
+        public System.Net.Sockets.TcpClient client;
+        public StreamWriter writer;
+        public StreamReader reader;
+        public string Name;
+        public string ipAddress;
+
+        public string Message;
+
+        public Thread ReceiveMessage;
+
+        public Communicator(System.Net.Sockets.TcpClient connection)
+        {
+            client = connection;
+            writer = new StreamWriter(client.GetStream());
+            reader = new StreamReader(client.GetStream());
+
+            ipAddress = reader.ReadLine();
+            Name = reader.ReadLine();
+
+            ReceiveMessage = new Thread(new ThreadStart(getMessage));
+            ReceiveMessage.IsBackground = true;
+            ReceiveMessage.Start();
+        }
+
+        public void getMessage()
+        {
+            while (true)
+            {
+                try
+                {
+                    Message = reader.ReadLine();
+                    if (reader.EndOfStream)
+                    {
+                        client.Close();
+                        ReceiveMessage.Abort();
+                    }
+                }
+                catch (IOException)
+                { client.Close(); }
+                catch (ObjectDisposedException)
+                { reader.Dispose(); }
+            }
+        }
+    }
+}
---
+++ Server/TranzapServer/Server.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TranzapServer
+{
+    class Server
+    {
+        private TcpListener tcpListener;
+        private string lanIP;
+        private string extIP;
+        
+    }
+}
